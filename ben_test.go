42427f8c317edd6691cde0f6e992ded455a85803
Alexander Fet Rødseth <rodseth@gmail.com>
2023-04-16 13:52:26 +0200

    Add a test

--- ben_test.go
+++ ben_test.go
@@ -1,9 +1,29 @@
 package ben
 
 import (
+	"os"
 	"reflect"
 	"testing"
+	"time"
 )
+
+func TestConvertToMIDI(t *testing.T) {
+	notes := []MidiNote{
+		{Frequency: 440, Duration: time.Second, Velocity: 127, Channel: 1, Instrument: 0, Slur: false},
+		{Frequency: 880, Duration: time.Second, Velocity: 127, Channel: 1, Instrument: 0, Slur: false},
+		{Frequency: 220, Duration: time.Second, Velocity: 127, Channel: 1, Instrument: 0, Slur: false},
+	}
+	tracks := [][]MidiNote{notes}
+	data, err := ConvertToMIDI(tracks)
+	if err != nil {
+		t.Errorf("ConvertToMIDI returned an error: %v", err)
+	}
+	// write to a file to verify that MIDI file is valid
+	err = os.WriteFile("test.mid", data, 0644)
+	if err != nil {
+		t.Errorf("Failed to write MIDI data to file: %v", err)
+	}
+}
 
 func TestProcessBenTrack(t *testing.T) {
 	benInput := "C^ D. F G! F. E^ D C"
@@ -19,7 +39,10 @@ func TestProcessBenTrack(t *testing.T) {
 		{Duration: TicksToDuration(96)},
 	}
 
-	midiNotes := ProcessBenTrack(benInput)
+	midiNotes, err := ParseBenTrack(benInput)
+	if err != nil {
+		t.Errorf("ParseBenTrack: %v", err)
+	}
 
 	if len(midiNotes) != len(expectedMidiNotes) {
 		t.Fatalf("Expected %d midi notes, got %d", len(expectedMidiNotes), len(midiNotes))
